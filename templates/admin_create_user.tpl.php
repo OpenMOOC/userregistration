4b68b1485e056043f3f343db2f8710ffe4f08aba
Jorge López Pérez <jorge@adobo.org>
2013-05-09 17:58:12 +0200

    Rename JS file

--- templates/admin_create_user.tpl.php
+++ templates/admin_create_user.tpl.php
@@ -9,7 +9,7 @@ $this->includeAtTemplateBase('includes/header.php');
 ?>
 <script type="text/javascript" src="resources/jquery-simplePassMeter/jquery.js"></script>
 <script type="text/javascript" src="resources/jquery-simplePassMeter/jquery.simplePassMeter-0.2b.js"></script>
-<script type="text/javascript" src="resources/genpw.js"></script>
+<script type="text/javascript" src="resources/userregistration.js"></script>
 <?php
 
 if(isset($this->data['error'])): ?>
